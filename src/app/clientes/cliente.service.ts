ebc134c25b4c68f3d81bd3cc54e03f048c0bf0bd
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-16 16:58:29 -0300

    Implementando HttpClient en el servicio de clientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,17 +2,25 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { CLIENTES } from './clientes.json';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string = "http://localhost:8080/api/clientes"
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>
   {
     //convierte en un flujo al arreglo
-    return of(CLIENTES);
+    //return of(CLIENTES);
+
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
   }
 }
