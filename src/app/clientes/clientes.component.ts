e63a2a3761aa8b4f2f6ecede68c0e338f90260e5
Bianca Pilegi <bpilegi@mobydigital.com>
2021-03-16 11:08:53 -0300

    Desacoplando el listado de clientes del componente clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,18 +8,13 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Bianca', apellido: 'Pilegi', email: 'pilegibianca@gmail.com', createAt: '2021-03-16'},
-    {id: 2, nombre: 'Pepe', apellido: 'García', email: 'pgarcia@gmail.com', createAt: '2021-03-10'},
-    {id: 3, nombre: 'Carlos', apellido: 'Bala', email: 'cbala@gmail.com', createAt: '2021-03-16'},
-    {id: 4, nombre: 'Susana', apellido: 'Gimenez', email: 'sgimenez@gmail.com', createAt: '2021-02-16'},
-    {id: 5, nombre: 'Ricardo', apellido: 'Fort', email: 'rfort@gmail.com', createAt: '2020-06-08'},
-    {id: 6, nombre: 'Julian', apellido: 'Weich', email: 'jweich@gmail.com', createAt: '2018-08-28'}
-  ];
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, nombre: 'Bianca', apellido: 'Pilegi', email: 'pilegibianca@gmail.com', createAt: '2021-03-16'},
+  {id: 2, nombre: 'Pepe', apellido: 'García', email: 'pgarcia@gmail.com', createAt: '2021-03-10'},
+  {id: 3, nombre: 'Carlos', apellido: 'Bala', email: 'cbala@gmail.com', createAt: '2021-03-16'},
+  {id: 4, nombre: 'Susana', apellido: 'Gimenez', email: 'sgimenez@gmail.com', createAt: '2021-02-16'},
+  {id: 5, nombre: 'Ricardo', apellido: 'Fort', email: 'rfort@gmail.com', createAt: '2020-06-08'},
+  {id: 6, nombre: 'Julian', apellido: 'Weich', email: 'jweich@gmail.com', createAt: '2018-08-28'}
+];
